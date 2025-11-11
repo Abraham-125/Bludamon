9bf34a92e75d124a6e235b4d0304dd46e278b25f
Abraham-125 <abrahamgutierrez1999@gmail.com>
2025-11-11 17:27:07 -0300

    first commit

---
+++ src/modulos/catalogo/Catalogo.tsx
@@ -0,0 +1,29 @@
+import "./Catalogo.css";
+import logo from "../../assets/logo.png";
+function Catalogo() {
+  return (
+    <>
+      <div className="catalogo-container">
+        <div className="row navbar-container">
+          <div className="bloque-1">
+            <a href="">
+              <img src={logo} className="logo" alt="logo bludamon" />
+            </a>
+
+            <button className="boton-carrito">Carrito</button>
+          </div>
+        </div>
+        <div className="row menu-container">
+          <div className="bloque-2 col-12 col-sm-2 ">
+            <button className="menu-boton">Poleras</button>
+            <button className="menu-boton boton-desactivado">Polerones</button>
+            <button className="menu-boton boton-desactivado">Visor</button>
+          </div>
+          <div className="bloque-3 col-12 col-sm-10 "> bloque 3</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Catalogo;
